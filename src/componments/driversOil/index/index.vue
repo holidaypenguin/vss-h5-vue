5f5dbf3c3602f8edf2763e91cd16621cabcfd9b9
holidaypenguin <songship1221@sina.com>
2019-11-21 20:31:46 +0800

    Add：加油站列表

--- src/componments/driversOil/index/index.vue
+++ src/componments/driversOil/index/index.vue
@@ -6,13 +6,13 @@
     <div class="p-index-top">
       <van-dropdown-menu class="p-index-distance"
         active-color="#00BE06">
-        <van-dropdown-item v-model="sort" :options="sortOpts"
+        <van-dropdown-item v-model="params.sort" :options="sortOpts"
           get-container=".p-index-top"
           @change="sortChangeHandler"/>
       </van-dropdown-menu>
       <van-dropdown-menu class="p-index-distance"
         active-color="#00BE06">
-        <van-dropdown-item v-model="oilNo" :options="oilNoOpts"
+        <van-dropdown-item v-model="params.oilNo" :options="oilNoOpts"
           @change="oilNoChangeHandler"/>
       </van-dropdown-menu>
       <div class="p-index-top-msg">在线支付</div>
@@ -21,39 +21,26 @@
     <div class="p-index-split"></div>
 
     <div class="p-index-list">
-      <div class="p-index-item">
+      <div class="p-index-item"
+        v-for="(item) in list"
+        :key="item.gasId">
         <div class="p-index-left">
-          <img class="p-index-icon" src=""/>
+          <img class="p-index-icon" :src="item.gasLogoSmall"/>
         </div>
         <div class="p-index-right">
-          <div class="p-index-name">北京广开源加油站</div>
-          <div class="p-index-addr">北京市朝阳区北四环路六公主坟村</div>
-          <div class="p-index-money">6.00/升</div>
+          <div class="p-index-name">{{item.gasName}}</div>
+          <div class="p-index-addr">{{item.gasAddress}}</div>
+          <div class="p-index-money">{{item.oilNo}}/升</div>
           <div class="p-index-other">
-            <div class="p-index-num">92#</div>
+            <div class="p-index-num">{{item.oilName}}#</div>
             <div class="p-index-status p-index-status--1"></div>
-            <div class="p-index-diff">0.4元</div>
+            <div class="p-index-diff">{{item.oil_92}}元</div>
           </div>
-          <div class="p-index-go">3.1KM</div>
-        </div>
-      </div>
-      <div class="p-index-item">
-        <div class="p-index-left">
-          <img class="p-index-icon" src=""/>
-        </div>
-        <div class="p-index-right">
-          <div class="p-index-name">北京广开源加油站</div>
-          <div class="p-index-addr">北京市朝阳区北四环路六公主坟村</div>
-          <div class="p-index-money">6.00/升</div>
-          <div class="p-index-other">
-            <div class="p-index-num">92#</div>
-            <div class="p-index-status p-index-status--1"></div>
-            <div class="p-index-diff">0.4元</div>
-          </div>
-          <div class="p-index-go">3.1KM</div>
+          <div class="p-index-go">{{item.dis}}KM</div>
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -62,9 +49,22 @@ import VanDropdownMenu from 'vant/lib/dropdown-menu'
 import 'vant/lib/dropdown-menu/style'
 import VanDropdownItem from 'vant/lib/dropdown-item'
 import 'vant/lib/dropdown-item/style'
+
 import {
   mapState,
+  mapMutations,
 } from 'vuex'
+
+import Dom from 'utils/dom'
+
+import {
+  SET_LOADING,
+  SET_LOADING_NEXT,
+} from '../../../module/driversOil/store/mutations-type'
+
+import {
+  GETLIST,
+} from '../../../module/driversOil/interface'
 
 import Utils from '@/module/driversOil/utils'
 import Nav from '../nav/nav.vue'
@@ -81,17 +81,44 @@ export default {
 
   data () {
     return {
-      sort: 1,
       sortOpts: [
         {text: '距离优先', value: 1},
         {text: '价格优先', value: 2},
       ],
-      oilNo: 92,
       oilNoOpts: [
         {text: '92号汽油', value: 92},
         {text: '95号汽油', value: 95},
         {text: '98号汽油', value: 98},
         {text: '0号柴油', value: 0},
+      ],
+      params: {
+        sort: 1,
+        oilNo: 92,
+        lng: 0,
+        lat: 0,
+      },
+      page: {
+        page: 1,
+        count: this.$store.state.pageSize,
+        gonext: false,
+        totalPage: 1,
+      },
+      getting: false,
+      listWrapEl: undefined,
+      listEl: undefined,
+      scrollTop: 0,
+      list: [
+        {gasId: 1, gasName: '1', gasAddress: '2', oilNo: '1', oilName: '2', oil_92: '1', dis: '1'},
+        {gasId: 2, gasName: '1', gasAddress: '2', oilNo: '1', oilName: '2', oil_92: '1', dis: '1'},
+        {gasId: 3, gasName: '1', gasAddress: '2', oilNo: '1', oilName: '2', oil_92: '1', dis: '1'},
+        {gasId: 4, gasName: '1', gasAddress: '2', oilNo: '1', oilName: '2', oil_92: '1', dis: '1'},
+        {gasId: 5, gasName: '1', gasAddress: '2', oilNo: '1', oilName: '2', oil_92: '1', dis: '1'},
+        {gasId: 6, gasName: '1', gasAddress: '2', oilNo: '1', oilName: '2', oil_92: '1', dis: '1'},
+        {gasId: 7, gasName: '1', gasAddress: '2', oilNo: '1', oilName: '2', oil_92: '1', dis: '1'},
+        {gasId: 8, gasName: '1', gasAddress: '2', oilNo: '1', oilName: '2', oil_92: '1', dis: '1'},
+        {gasId: 9, gasName: '1', gasAddress: '2', oilNo: '1', oilName: '2', oil_92: '1', dis: '1'},
+        {gasId: 10, gasName: '1', gasAddress: '2', oilNo: '1', oilName: '2', oil_92: '1', dis: '1'},
+        {gasId: 11, gasName: '1', gasAddress: '2', oilNo: '1', oilName: '2', oil_92: '1', dis: '1'},
       ],
     }
   },
@@ -106,8 +133,91 @@ export default {
   },
 
   async mounted () {
+    await this.search()
+    this.$nextTick(() => {
+      this.listWrapEl = this.$parent.$el
+      this.listEl = this.$el
+      this.nextPage()
+    })
   },
+
+  activated () {
+    this.$nextTick(() => {
+      this.listWrapEl && this.listWrapEl.scrollTo(0, this.scrollTop || 0)
+    })
+  },
+
   methods: {
+    ...mapMutations([
+      SET_LOADING,
+      SET_LOADING_NEXT,
+    ]),
+    nextPage () {
+      Dom.on(this.listWrapEl, 'scroll', this.addData)
+    },
+    addData () {
+      const scrollTop = parseFloat(this.listWrapEl.scrollTop)
+      this.scrollTop = scrollTop
+
+      if (!this.page.gonext) return
+
+      const clientHeight = parseFloat(this.listWrapEl.clientHeight)
+      const mainHeight = parseFloat(this.listEl.clientHeight)
+      const totalheight = clientHeight + scrollTop // 浏览器的高度加上滚上去的高度
+
+      if (totalheight >= mainHeight - 200) {
+        // 当文档的高度小于或者等于总的高度加100的时候，开始动态加载数据
+        this.$nextTick(() => {
+          this.search(this.page.page + 1)
+        })
+      }
+    },
+    async search (pageNo = 1) {
+      if (this.getting) return
+
+      this.getting = true
+
+      if (pageNo !== 1 && pageNo > this.page.totalPage) {
+        this.page.gonext = false
+        this.getting = false
+
+        return
+      }
+      this[pageNo === 1 ? SET_LOADING : SET_LOADING_NEXT](true)
+
+      const {page, count, data} = await this.$axios.post(GETLIST, Object.assign(this.params, {
+        count: this.page.count,
+        page: pageNo,
+      })).catch((e) => {
+        this.getting = false
+        this[pageNo === 1 ? SET_LOADING : SET_LOADING_NEXT](false)
+
+        return Promise.reject(e)
+      })
+
+      if (pageNo === 1) {
+        this.list = []
+      }
+      this.list = this.list.concat(data)
+
+      this.page.page = pageNo
+      this.page.totalPage = parseInt((count + this.page.count - 1) / page.count, 10) || 0
+      this.page.gonext = data.length >= this.page.count
+
+      this.getting = false
+      this[pageNo === 1 ? SET_LOADING : SET_LOADING_NEXT](false)
+    },
+    goBack () {
+      this.$router.go(-1)
+    },
+    itemClickHandler (gasId) {
+      this.$router.push({
+        name: 'detail',
+        params: {
+          gasId,
+        },
+      })
+    },
     sortChangeHandler () {},
     oilNoChangeHandler () {},
   },
@@ -117,10 +227,17 @@ export default {
 <style lang="postcss">
 @n p{
   @b index{
+    overflow: auto;
     @e top{
       margin-top: 88px;
       padding: 14px 32px;
       display: flex;
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      z-index: 10;
+      background: #FFFFFF;
       @e msg{
         flex: 1;
         text-align: right;
@@ -145,10 +262,13 @@ export default {
       }
     }
     @e split{
+      margin-top: 188px;
       height: 16px;
       background: #F2F2F7;
     }
-    @e list{}
+    @e list{
+      padding-bottom: 40px;
+    }
     @e item{
       padding: 32px;
       display: flex;
@@ -167,6 +287,7 @@ export default {
       width: 100px;
       height: 100px;
       display: block;
+      border-radius: 12 px;
     }
     @e right{
       flex: 1;
--- src/module/driversOil/interface.js
+++ src/module/driversOil/interface.js
@@ -1,11 +1,17 @@
 /**
- * 登录 获取微信配置
+ * 获取油站列表
  */
-export const GETWXCONFIG = 'https://api.driversite.cn/share/getWxCgiConfig'
-// export const GETWXCONFIG = '/share/getWxCgiConfig'
+export const GETLIST = 'https://api.driversite.cn/czb/getGas'
+// export const GETLIST = '/czb/getGas'
 
 /**
- * 获取分享信息
+ * 获取油站详情
  */
-export const GETSHAREFEED = 'https://api.driversite.cn/share/getShareFeed'
-// export const GETSHAREFEED = '/share/getShareFeed'
+export const GETDETAIL = 'https://api.driversite.cn/czb/getGasDetail'
+// export const GETDETAIL = '/czb/getGasDetail'
+
+/**
+ * 获取用户订单列表
+ */
+export const GETORDER = 'https://api.driversite.cn/czb/getGasOrders'
+// export const GETORDER = '/czb/getGasOrders'
