5bff6c6566142009b820cc119b94bb83914f35a9
holidaypenguin <songship1221@sina.com>
2020-01-15 10:12:40 +0800

    Update：司机圈儿

--- src/module/driversRing/app.vue
+++ src/module/driversRing/app.vue
@@ -11,9 +11,9 @@
         <div class="next scale25"></div>
       </div>
       <div class="item item-2">
-        <div class="title">吐露心声相互交流</div>
-        <div class="msg">拒绝沉默，讨论网约车运行中发现的问题</div>
-        <div class="msg">相互交流，学习更多的技巧</div>
+        <div class="title">专家解决网约车问题</div>
+        <div class="msg">滴滴、首约、曹操、美团等网约车平台专家</div>
+        <div class="msg">入驻司机圈儿平台，为您解决日常问题</div>
         <div class="phone"></div>
         <div class="next scale25"></div>
         <div class="bottom">
@@ -26,9 +26,9 @@
         </div>
       </div>
       <div class="item item-3">
-        <div class="title">工作、娱乐一个都不能少</div>
-        <div class="msg">拒绝平庸乏味，生活并不只有工作</div>
-        <div class="msg">不再为每次的堵车心烦</div>
+        <div class="title">加油优惠力度大</div>
+        <div class="msg">加油优惠力度大，为您节省</div>
+        <div class="msg">最高 <span class="msg-red">6</span> 折优惠</div>
         <div class="phone"></div>
         <div class="bottom">
           <div class="logo"></div>
